2f4312cd517e121d5d2005df3486a1fba2f077ae
phuonganh412 <phanhhh0412@gmail.com>
2026-03-19 00:53:56 +1100

    refactor SocialLinks components

--- src/components/SocialLinks/SocialLinkItem.jsx
+++ src/components/SocialLinks/SocialLinkItem.jsx
@@ -1,12 +1,13 @@
-export function SocialLinkItem({ label, href, icon: Icon }) {
+import { SocialIcon } from "components/SocialIcon";
+
+export function SocialLinkItem({ label, href, icon }) {
     return (
         <a
             href={href}
             aria-label={label}
             className="inline-flex h-9 w-9 items-center justify-center rounded-md text-white/80 transition-colors hover:text-white"
         >
-            {" "}
-            <Icon className="h-5 w-5" aria-hidden />{" "}
+            <SocialIcon icon={icon} />
         </a>
     );
 }
