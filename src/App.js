b642ba018924940323b4b8177446468831c017ca
phuonganh412 <phanhhh0412@gmail.com>
2026-03-24 11:32:04 +1100

    add reservation page with only header and footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import { Header } from "components/Header";
 import { Main } from "components/Main";
 import { Footer } from "components/Footer";
+import { Reservation } from "components/Reservation";
+import { RESERVATION_PATH } from "constants/routes";
 
 function App() {
+    if (window.location.pathname === RESERVATION_PATH) {
+        return <Reservation />;
+    }
+
     return (
         <div className="App">
             <Header />
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "components/Header";
+import { Footer } from "components/Footer";
+
+export function Reservation() {
+    return (
+        <div className="App">
+            <Header />
+            <Footer />
+        </div>
+    );
+}
